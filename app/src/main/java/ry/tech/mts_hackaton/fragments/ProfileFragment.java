aa151d5d81b16c56652996d03c0acf8f14a48b69
jxpix <wouldress@gmail.com>
2025-03-18 18:01:37 +0300

    New interface #3

--- app/src/main/java/ry/tech/mts_hackaton/fragments/ProfileFragment.java
+++ app/src/main/java/ry/tech/mts_hackaton/fragments/ProfileFragment.java
@@ -5,14 +5,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import ry.tech.mts_hackaton.R;
 
 public class ProfileFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.buttonBack).setOnClickListener(v -> {
+            // Обработка нажатия кнопки "Назад"
+            requireActivity().onBackPressed();
+        });
+
+        view.findViewById(R.id.buttonEdit).setOnClickListener(v -> {
+            // Обработка нажатия кнопки редактирования
+        });
     }
 }
